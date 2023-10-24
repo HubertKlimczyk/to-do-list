f3f3354525e32af1ddc4cedbefdd347849c7218e
Hubert Klimczyk <hklimczyk1@gmail.com>
2023-10-24 19:44:12 +0200

    remove tasks

--- script/script.js
+++ script/script.js
@@ -10,20 +10,6 @@
         },
     ];
 
-    const render = () => {
-        let htmlString = "";
-
-        for (const task of tasks) {
-            htmlString += `
-                <li${task.done ? " style=\"text-decoration: line-through\"" : ""}>
-                    ${task.content}
-                </li>
-            `;
-        }
-
-        document.querySelector(".js-list").innerHTML = htmlString;
-
-    };
 
     const addNewTask = (newTaskContent) => {
         tasks.push({
@@ -32,6 +18,40 @@
 
         render();
     };
+
+    const removeTask = (taskIndex) => {
+        tasks.splice(taskIndex, 1);
+        render();
+    };
+
+
+    const render = () => {
+        let htmlString = "";
+
+        for (const task of tasks) {
+            htmlString += `
+                <li
+                    ${task.done ? " style=\"text-decoration: line-through\"" : ""}>
+                    <button class="js-remove">usuń</button>
+                    ${task.content}
+                </li>
+            `;
+        }
+
+        document.querySelector(".js-list").innerHTML = htmlString;
+
+
+        const removeButtons = document.querySelectorAll(".js-remove");
+
+        removeButtons.forEach((removeButton, index) => {
+            removeButton.addEventListener("click", () => {
+                removeTask(index);
+            });
+        });
+
+    };
+
+
 
     const onFormSubmit = (event) => {
         event.preventDefault();
